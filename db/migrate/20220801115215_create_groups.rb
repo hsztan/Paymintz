14d4e8978a6cafe0331b88a123a6e3847802507c
Henry Nawrocki <hdev@sztanski.com>
2022-08-01 07:27:42 -0500

    Add relations has-and-belong-to-many relations between groups and payments

--- db/migrate/20220801115215_create_groups.rb
+++ db/migrate/20220801115215_create_groups.rb
@@ -2,8 +2,8 @@ class CreateGroups < ActiveRecord::Migration[7.0]
   def change
     create_table :groups do |t|
       t.references :user, null: false, foreign_key: true
-      t.string :name=string
-      t.string :icon=string
+      t.string :name
+      t.string :icon
 
       t.timestamps
     end
