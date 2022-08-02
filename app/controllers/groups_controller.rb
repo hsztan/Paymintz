01ea0685a7815031ba0187c1ed6d44c611136d4b
Henry Nawrocki <hdev@sztanski.com>
2022-08-02 12:47:07 -0500

    Category show page displays payments

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -8,7 +8,9 @@ class GroupsController < ApplicationController
   end
 
   # GET /groups/1 or /groups/1.json
-  def show; end
+  def show
+    @payments = @group.payments
+  end
 
   # GET /groups/new
   def new
