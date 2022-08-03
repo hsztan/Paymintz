fe67636ded15d35ce0b7a6471545cdf22f67465d
Henry Nawrocki <hdev@sztanski.com>
2022-08-03 10:19:46 -0500

    Adds total price amount of transaction to category total amount

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,8 @@
 class Group < ApplicationRecord
   belongs_to :user
   has_and_belongs_to_many :payments, join_table: 'groups_payments'
+
+  def transactions_total
+    payments.sum(:amount)
+  end
 end
