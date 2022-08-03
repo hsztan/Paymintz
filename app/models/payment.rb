b273fb79d88ccb13560cb0cf393845822131f92d
Henry Nawrocki <hdev@sztanski.com>
2022-08-03 17:59:33 -0500

    Add validations to models

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,4 +1,7 @@
 class Payment < ApplicationRecord
   belongs_to :user
   has_and_belongs_to_many :groups, join_table: 'groups_payments'
+
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :amount, presence: true, numericality: { greater_than: 0, alert: 'must be greater than 0' }
 end
