21a64140ef2a4e6272dd6a176f8009d5f09da27b
Henry Nawrocki <hdev@sztanski.com>
2022-08-03 11:05:48 -0500

    Successfully destroys transactions

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -59,9 +59,10 @@ class PaymentsController < ApplicationController
   # DELETE /payments/1 or /payments/1.json
   def destroy
     @payment.destroy
+    @group = Group.find(params[:group_id])
 
     respond_to do |format|
-      format.html { redirect_to payments_url, notice: 'Payment was successfully destroyed.' }
+      format.html { redirect_to group_url(@group), notice: 'Payment was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   resources :groups, only: [] do
-    resources :payments, only: [:new, :create]
+    resources :payments, only: [:new, :create, :destroy]
   end
 
   # Defines the root path route ("/")
