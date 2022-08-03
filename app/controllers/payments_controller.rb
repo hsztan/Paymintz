98683ef22b45c2500c060008d83f6f58a116eb2c
Henry Nawrocki <hdev@sztanski.com>
2022-08-03 14:42:33 -0500

    Redirect to splash page in all controllers if not logged in

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,4 +1,5 @@
 class PaymentsController < ApplicationController
+  before_action :redirect_to_splash, unless: :signed_in?
   before_action :set_payment, only: %i[show edit update destroy]
 
   # GET /payments or /payments.json
